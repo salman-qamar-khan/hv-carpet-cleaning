8c07c2bd03ed703536e7ddd782ae3dbb31c09aa3
Salman Qamar Khan <work.salmanqamarkhan@gmail.com>
2019-01-23 23:01:55 +0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -23,13 +23,13 @@ define('WP_CACHE', true); // Added by W3 Total Cache
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'hvcarpet_carpeting');
+define('DB_NAME', '');
 
 /** MySQL database username */
-define('DB_USER', 'hvcarpet_user');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'An1mat3d88');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');

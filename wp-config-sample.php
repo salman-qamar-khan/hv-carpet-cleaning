bc8042c8261e5ffd959e414a933d3ccae4d3a0a4
Salman Qamar Khan <work.salmanqamarkhan@gmail.com>
2019-01-29 23:55:00 +0500

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -26,7 +26,7 @@ define('DB_NAME', '');
 define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'password_he');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');

52c3c5319a5f173b4c13b7a3e4590481c18470f4
Salman Qamar Khan <work.salmanqamarkhan@gmail.com>
2019-02-13 16:11:11 +0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -36,7 +36,6 @@ define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
-
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 

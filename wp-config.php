449fd8c7c426d1974fe1139b1c947404a15dd40f
Salman Qamar Khan <work.salmanqamarkhan@gmail.com>
2019-02-14 21:35:54 +0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -32,7 +32,7 @@ define('DB_USER', '');
 define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'loc');
+define('DB_HOST', 'lo');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
